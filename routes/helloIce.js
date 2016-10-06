fca8182d947b5166939fe65ec9b9275e8a2d230f
Benson <benson@gosmio.biz>
2016-10-06 20:43:50 +0800

    helloIce 加上, Server Side Test

--- routes/helloIce.js
+++ routes/helloIce.js
@@ -9,4 +9,28 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, '../views/helloIce.html'));  
 });
 
+router.get('/bravoLogin', function(req, res, next) {
+  // 登入 Brave Web Site
+  var websiteURL = 'https://www.rd2.atcity.dev';
+  var DeviceId = "Benson's Nodejs";
+
+  var BravoLogin = require('../public/bravoLogin').BravoLogin;
+  var bravoLogin = new BravoLogin(DeviceId);
+  bravoLogin.setWebsite(websiteURL);
+  bravoLogin.createSession(true).then(
+      function () {
+        localStorage.setItem("loginInfo", JSON.stringify(bravoLogin.loginInfo));
+        // 登入成功
+        console.log("登入成功");
+      },
+      function (msg) {
+        console.log(msg);
+        // 登入失敗
+      }
+  );
+
+  console.log(BravoLogin);
+  res.send("login to"+websiteURL);
+});
+
 module.exports = router;
