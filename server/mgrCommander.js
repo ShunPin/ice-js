a40ca7c88b54026034d915f7b9da7d2e81907fbe
benson <icebro@gmail.com>
2016-12-25 21:53:09 +0800

    修正大小寫, 以符合慣例

--- server/mgrCommander.js
+++ server/mgrCommander.js
@@ -7,7 +7,7 @@
 // <> 初始化, setWebsite
 // <> get/set/del (設定), 並對應 Id 記錄起來, 並更新 Commander
 
-'use strict'
+'use strict';
 
 // 開發測試用
 //var Commander = require('../server/stressTestCommander');
@@ -15,8 +15,8 @@
 // 正常用
 var Commander = require('../server/stressLoginCommander');
 
-function helper() {
-    this.init = function() {
+function ClassHelper() {
+    this.init = function () {
         this.model = {};
         this._settings = {};
         this._commanders = {};
@@ -30,37 +30,33 @@ function helper() {
  *  設定要登入的 Website URL
  * @param url
  */
-helper.prototype.setWebsite = function (url) {
+ClassHelper.prototype.setWebsite = function (url) {
     // 初始化
     this._settings.Website = url;
 };
 
 
-helper.prototype.get = function (id, callback){
+ClassHelper.prototype.get = function (id, callback) {
     // 有指定 id 則查詢
-    if (id)
-    {
-        if (this.model.hasOwnProperty(id))
-        {
+    if (id) {
+        if (this.model.hasOwnProperty(id)) {
             var value = this.model[id];
-            if (callback instanceof Function) callback(null,value);
+            if (callback instanceof Function) callback(null, value);
         }
-        else
-        {
-            if (callback instanceof Function) callback(new Error('Not Found Error!'),null);
+        else {
+            if (callback instanceof Function) callback(new Error('Not Found Error!'), null);
         }
     }
     // 無指定 id 則, 全部提供
     else {
-        if (callback instanceof Function) callback(null,Object.values(this.model));
+        if (callback instanceof Function) callback(null, Object.values(this.model));
     }
 };
 
 // 設定設定資訊, 非同步方法
-helper.prototype.set = function (id, value, callback){
+ClassHelper.prototype.set = function (id, value, callback) {
     // 必須指定 id
-    if (id)
-    {
+    if (id) {
         this.model[id] = value;
 
         if (this._commanders.hasOwnProperty(id) == false) {
@@ -78,44 +74,41 @@ helper.prototype.set = function (id, value, callback){
             cmder.stop();
         }
 
-        if (callback instanceof Function) callback(null,value);
+        if (callback instanceof Function) callback(null, value);
     }
     else {
-        if (callback instanceof Function) callback(new Error("No provide id Error!"),null);
+        if (callback instanceof Function) callback(new Error("No provide id Error!"), null);
     }
 };
 
 // 刪除設定資訊, 非同步方法
-helper.prototype.del = function (id, callback){
+ClassHelper.prototype.del = function (id, callback) {
     // 必須指定 id
-    if (id)
-    {
-        if (this.model.hasOwnProperty(id))
-        {
+    if (id) {
+        if (this.model.hasOwnProperty(id)) {
             delete this.model[id];
             var cmder = this._commanders[id];
             cmder.stop();
             delete this._commanders[id];
 
-            if (callback instanceof Function) callback(null,null);
+            if (callback instanceof Function) callback(null, null);
         }
-        else
-        {
-            if (callback instanceof Function) callback(new Error('Not Found Error!'),null);
+        else {
+            if (callback instanceof Function) callback(new Error('Not Found Error!'), null);
         }
     }
     else {
-        if (callback instanceof Function) callback(new Error("No provide id Error!"),null);
+        if (callback instanceof Function) callback(new Error("No provide id Error!"), null);
     }
 };
 
 
-helper.prototype.getCommanders = function () {
+ClassHelper.prototype.getCommanders = function () {
     var values = [];
 
-    for (var k in this._commanders){
+    for (var k in this._commanders) {
         if (this._commanders.hasOwnProperty(k)) {
-            console.log("Key is " + k + ", value is " + this._commanders[k].status.id);
+            //console.log("Key is " + k + ", value is " + this._commanders[k].status.id);
             values.push(this._commanders[k].status);
         }
     }
@@ -126,17 +119,17 @@ helper.prototype.getCommanders = function () {
 /* ************************************************************************
  SINGLETON CLASS DEFINITION
  ************************************************************************ */
-helper.instance = null;
+ClassHelper.instance = null;
 
 /**
  * Singleton getInstance definition
  * @return singleton class
  */
-helper.getInstance = function () {
+ClassHelper.getInstance = function () {
     if (this.instance === null) {
-        this.instance = new helper();
+        this.instance = new ClassHelper();
     }
     return this.instance;
-}
+};
 
-module.exports = helper.getInstance();
+module.exports = ClassHelper.getInstance();
