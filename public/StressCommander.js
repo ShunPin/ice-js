a0abe628f3727292c84106b1d4fab8f452c4a8e2
benson <icebro@gmail.com>
2016-12-25 21:57:34 +0800

    Ice.Class 使用上有問題, 改用一般 JS 類別寫法

--- public/StressCommander.js
+++ public/StressCommander.js
@@ -14,7 +14,6 @@
 // 另外使用 Promise 方式 Callback
 
 var log = require("log4js").getLogger("stress");
-var Ice = require("ice").Ice;
 
 /**
  * 擴充 String.format
@@ -31,125 +30,248 @@ if (!String.prototype.format) {
     };
 }
 
-var commander = Ice.Class({
-    Config : {},
-    status : {
-        running : false,
-        targetCount : 0,
-        currentCount : 0,
-        finishCount : 0,
-        failCount : 0,
-    },
+function Commander(config) {
+    this.Config = config;
+    this.runIndex = -1;
+    this.intervalID = undefined;    // Timer ID
+    this.status = {};
+    this.status.id = config.id;
+    this.status.running = false;
+    this.status.targetCount = config.targetCount;
+    this.status.currentCount = 0;
+    this.status.finishCount = 0;
+    this.status.failCount = 0;
+}
 
-    __init__: function (config) {
-        this.Config = config;
-        this.runIndex = -1;
-        this.intervalID = undefined;    // Timer ID
-    },
+// 回報運行狀態
+Commander.prototype.report = function () {
+    return JSON.stringify(this.status);
+};
 
-    // 回報運行狀態
-    report : function () {
-        return JSON.stringify(this.status);
-    },
+/**
+ *  要測試 Run 的 constructor
+ *  必須 Override
+ * @returns {Object}
+ */
+Commander.prototype.createRunner = function () {
+    log.error("[Error] you must implement {0}:{1}".format(self.constructor.name,arguments.callee.toString()));
+    return undefined;
+};
 
-    /**
-     *  要測試 Run 的 constructor
-     *  必須 Override
-     * @returns {Object}
-     */
-    createRunner : function () {
-        log.error("[Error] you must implement {0}:{1}".format(self.constructor.name,arguments.callee.toString()));
-        return undefined;
-    },
+/**
+ *  要測試 Run 的 啟動 action
+ *  必須 Override
+ * @param runner
+ */
+Commander.prototype.runAction = function (runner) {
+    log.error("[Error] you must implement {0}:{1}".format(self.constructor.name,arguments.callee.toString()));
+};
 
-    /**
-     *  要測試 Run 的 啟動 action
-     *  必須 Override
-     * @param runner
-     */
-    runAction : function (runner) {
-        log.error("[Error] you must implement {0}:{1}".format(self.constructor.name,arguments.callee.toString()));
-    },
+// start 測試開始
+Commander.prototype.start = function () {
+    if (this.status.running) return;
 
-    // start 測試開始
-    start: function () {
-        if (this.status.running) return;
+    // 開始
+    this._begin();
 
-        // 開始
-        this._begin();
+    // 啟動第一隻機器人
+    this._each();
 
-        // 啟動第一隻機器人
-        this._each();
+};
 
-    },
+// stop 測試停止
+Commander.prototype.stop = function() {
+    if (this.status.running == false) return;
 
-    // stop 測試停止
-    stop: function() {
-        if (this.status.running == false) return;
+    log.info("StressCommander [STOP]");
+    this._stop();
+};
 
-        log.info("StressCommander [STOP]");
-        this._stop();
-    },
+// 當 runner 任務完成時呼叫
+Commander.prototype.success = function (runner) {
+    log.info("StressCommander [SUCCESS],{0}#{1}".format("任務完成機器人",runner.runnerIndex));
 
-    // 當 runner 任務完成時呼叫
-    success: function (runner) {
-        log.info("StressCommander [SUCCESS],{0}#{1}".format("任務完成機器人",runner.runnerIndex));
+    this.status.finishCount++;
+    this.status.currentCount--;
 
-        this.status.finishCount++;
-        this.status.currentCount--;
+    log.info("StressCommander [STATUS]", JSON.stringify(this.status));
+};
 
-        log.info("StressCommander [STATUS]", JSON.stringify(this.status));
-    },
+// 當 runner 任務失敗時呼叫
+Commander.prototype.fail = function (runner) {
+    log.info("StressCommander [FAIL],{0}#{1}".format("任務失敗機器人",runner.runnerIndex));
 
-    // 當 runner 任務失敗時呼叫
-    fail: function (runner) {
-        log.info("StressCommander [FAIL],{0}#{1}".format("任務失敗機器人",runner.runnerIndex));
+    this.status.failCount++;
+    this.status.currentCount--;
 
-        this.status.failCount++;
-        this.status.currentCount--;
+    log.info("StressCommander [STATUS]", JSON.stringify(this.status));
+};
 
-        log.info("StressCommander [STATUS]", JSON.stringify(this.status));
-    },
+// begin() 開始
+Commander.prototype._begin = function () {
+    log.info("StressCommander [Begin],{0}={1}".format("TargetNumber",this.Config.targetCount));
 
-    // begin() 開始
-    _begin : function () {
-        log.info("StressCommander [Begin],{0}={1}".format("TargetNumber",this.Config.targetCount));
+    this.status.running = true;
+    this.status.targetCount = this.Config.targetCount;
+    this.status.currentCount = 0;
+    this.status.finishCount = 0;
+    this.status.failCount = 0;
+    this.runIndex = 0;
+    this.intervalID = setInterval(this._each.bind(this), this.Config.interval);
+};
 
-        this.status.running = true;
-        this.status.targetCount = this.Config.targetCount;
-        this.status.currentCount = 0;
-        this.status.finishCount = 0;
-        this.status.failCount = 0;
-        this.runIndex = 0;
-        this.intervalID = setInterval(this._each.bind(this), this.Config.interval);
-    },
-
-    // 還沒達到預定數量時，會依照速度設定呼叫
-    _each : function () {
-        // 檢查是否達到數量
-        if (this.status.currentCount < this.status.targetCount)
-        {
-            log.info("StressCommander [Join],{0}#{1}".format("加入機器人",this.runIndex));
-            var runner = this.createRunner();
-            if (runner !=null && runner != undefined) {
-                runner.runnerIndex = this.runIndex;
-                this.runAction(runner);
-                this.status.currentCount++;
-                this.runIndex++;
-            }
+// 還沒達到預定數量時，會依照速度設定呼叫
+Commander.prototype._each = function () {
+    // 檢查是否達到數量
+    if (this.status.currentCount < this.status.targetCount)
+    {
+        log.info("StressCommander [Join],{0}#{1}".format("加入機器人",this.runIndex));
+        var runner = this.createRunner();
+        if (runner !=null && runner != undefined) {
+            runner.runnerIndex = this.runIndex;
+            this.runAction(runner);
+            this.status.currentCount++;
+            this.runIndex++;
         }
-    },
+    }
+};
 
-    // 停止後處理
-    _stop : function () {
-        if (this.intervalID != undefined)
-        {
-            clearInterval(this.intervalID);
-            this.intervalID = undefined;
-        }
-        this.status.running = false;
-    },
-});
+// 停止後處理
+Commander.prototype._stop = function () {
+    if (this.intervalID != undefined)
+    {
+        clearInterval(this.intervalID);
+        this.intervalID = undefined;
+    }
+    this.status.running = false;
+};
 
-module.exports = commander;
+
+// var Commander = Ice.Class({
+//     Config : {},
+//     status : {
+//         id : 0,
+//         running : false,
+//         targetCount : 0,
+//         currentCount : 0,
+//         finishCount : 0,
+//         failCount : 0,
+//     },
+//
+//     __init__: function (config) {
+//         this.Config = config;
+//         this.runIndex = -1;
+//         this.intervalID = undefined;    // Timer ID
+//         this.status.id = config.id;
+//         this.status.targetCount = config.targetCount;
+//         this.status.currentCount = 0;
+//         this.status.finishCount = 0;
+//         this.status.failCount = 0;
+//     },
+//
+//     // 回報運行狀態
+//     report : function () {
+//         return JSON.stringify(this.status);
+//     },
+//
+//     /**
+//      *  要測試 Run 的 constructor
+//      *  必須 Override
+//      * @returns {Object}
+//      */
+//     createRunner : function () {
+//         log.error("[Error] you must implement {0}:{1}".format(self.constructor.name,arguments.callee.toString()));
+//         return undefined;
+//     },
+//
+//     /**
+//      *  要測試 Run 的 啟動 action
+//      *  必須 Override
+//      * @param runner
+//      */
+//     runAction : function (runner) {
+//         log.error("[Error] you must implement {0}:{1}".format(self.constructor.name,arguments.callee.toString()));
+//     },
+//
+//     // start 測試開始
+//     start: function () {
+//         if (this.status.running) return;
+//
+//         // 開始
+//         this._begin();
+//
+//         // 啟動第一隻機器人
+//         this._each();
+//
+//     },
+//
+//     // stop 測試停止
+//     stop: function() {
+//         if (this.status.running == false) return;
+//
+//         log.info("StressCommander [STOP]");
+//         this._stop();
+//     },
+//
+//     // 當 runner 任務完成時呼叫
+//     success: function (runner) {
+//         log.info("StressCommander [SUCCESS],{0}#{1}".format("任務完成機器人",runner.runnerIndex));
+//
+//         this.status.finishCount++;
+//         this.status.currentCount--;
+//
+//         log.info("StressCommander [STATUS]", JSON.stringify(this.status));
+//     },
+//
+//     // 當 runner 任務失敗時呼叫
+//     fail: function (runner) {
+//         log.info("StressCommander [FAIL],{0}#{1}".format("任務失敗機器人",runner.runnerIndex));
+//
+//         this.status.failCount++;
+//         this.status.currentCount--;
+//
+//         log.info("StressCommander [STATUS]", JSON.stringify(this.status));
+//     },
+//
+//     // begin() 開始
+//     _begin : function () {
+//         log.info("StressCommander [Begin],{0}={1}".format("TargetNumber",this.Config.targetCount));
+//
+//         this.status.running = true;
+//         this.status.targetCount = this.Config.targetCount;
+//         this.status.currentCount = 0;
+//         this.status.finishCount = 0;
+//         this.status.failCount = 0;
+//         this.runIndex = 0;
+//         this.intervalID = setInterval(this._each.bind(this), this.Config.interval);
+//     },
+//
+//     // 還沒達到預定數量時，會依照速度設定呼叫
+//     _each : function () {
+//         // 檢查是否達到數量
+//         if (this.status.currentCount < this.status.targetCount)
+//         {
+//             log.info("StressCommander [Join],{0}#{1}".format("加入機器人",this.runIndex));
+//             var runner = this.createRunner();
+//             if (runner !=null && runner != undefined) {
+//                 runner.runnerIndex = this.runIndex;
+//                 this.runAction(runner);
+//                 this.status.currentCount++;
+//                 this.runIndex++;
+//             }
+//         }
+//     },
+//
+//     // 停止後處理
+//     _stop : function () {
+//         if (this.intervalID != undefined)
+//         {
+//             clearInterval(this.intervalID);
+//             this.intervalID = undefined;
+//         }
+//         this.status.running = false;
+//     },
+// });
+
+module.exports = Commander;
     
