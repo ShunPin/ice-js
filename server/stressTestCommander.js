3cc5d74da23f7be830b63d22fe65ca10882771de
benson <icebro@gmail.com>
2016-12-25 22:46:19 +0800

    加入 stressTestCommander.js 測試用的 commander

---
+++ server/stressTestCommander.js
@@ -0,0 +1,40 @@
+/**
+ * Created by benson on 2016/12/24.
+ */
+
+const _super = require('../public/StressCommander').prototype;
+
+var method = Commander.prototype = Object.create(_super);
+
+function Commander() {
+    _super.constructor.apply(this, arguments);
+}
+
+method.setWebsite = function (url) {
+    this._settings.Website = url;
+};
+
+method.createRunner = function () {
+    //var self = this;
+
+    var Ice = require("Ice").Ice;
+    var runner = new Ice.Promise();
+
+    return runner;
+};
+
+method.runAction = function (runner) {
+    var self = this;
+    //var Ice = require("Ice").Ice;
+    //var isGuestLogin = (self.Config.method == 'GuestLogin') ? true : false;
+    var stayTime = self.Config.stayTime * 1000;
+    //var setting = self.Config;
+
+    var timerID = setInterval(function () {
+        runner.succeed();
+        self.success(runner);
+        clearInterval(timerID);
+    }, stayTime);
+};
+
+module.exports = Commander;
